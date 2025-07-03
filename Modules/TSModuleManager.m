7f91ea5cd9657865e8f21752c3b8076d4cf6b445
a1109471307 <50288272+a1109471307@users.noreply.github.com>
2025-07-03 18:04:40 +0800

    Create TSModuleManager.m

---
+++ Modules/TSModuleManager.m
@@ -0,0 +1,11 @@
+#import "TSModuleManager.h"
+
+@interface TSModuleManager ()
+@property (nonatomic, strong) NSMutableDictionary<NSNumber *, id<TSModuleProtocol>> *modules;
+@end
+
+@implementation TSModuleManager
+
+// 实现共享实例、注册、执行等方法（完整实现参考之前的代码）
+
+@end
